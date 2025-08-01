34425660458ca41b565d978360a52088f19bca38
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2025-08-01 16:06:30 +0700

    chore: update locale configurations to use environment variables for better flexibility

--- config/app.php
+++ config/app.php
@@ -83,33 +83,11 @@ return [
     |
     */
 
-    'locale' => 'id',
+    'locale' => env('APP_LOCALE', 'id'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Fallback Locale
-    |--------------------------------------------------------------------------
-    |
-    | The fallback locale determines the locale to use when the current one
-    | is not available. You may change the value to correspond to any of
-    | the language folders that are provided through your application.
-    |
-    */
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'id'),
 
-    'fallback_locale' => 'id',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Faker Locale
-    |--------------------------------------------------------------------------
-    |
-    | This locale will be used by the Faker PHP library when generating fake
-    | data for your database seeds. For example, this will be used to get
-    | localized telephone numbers, street address information and more.
-    |
-    */
-
-    'faker_locale' => 'en_US',
+    'faker_locale' => env('APP_FAKER_LOCALE', 'id_ID'),
 
     /*
     |--------------------------------------------------------------------------
